a42c2251a3cfd66768580ed0e8a0dc5635213a90
fukukei23 <fukukei23@users.noreply.github.com>
2025-11-28 23:37:43 +0900

    Update browser_use_agent.py (Stage 4: NavigationDriver generalization)

--- app/agents/browser_use_agent.py
+++ app/agents/browser_use_agent.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d17db0748608f991197c24be11be5c3f883223b3451dbbf1130c56c29be7620f
-size 117132
+oid sha256:4b2330ebb26f2f62e9421f2cdd4002ea998185d33e18cc2cae09aea54bad1203
+size 122533
